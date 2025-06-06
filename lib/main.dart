781317ebfbb05fecc3c8bef2215ad91bb69d53a2
firas <firashedfi4@gmail.com>
2025-06-06 22:03:27 +0100

    Some updates

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: darkTheme,
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
